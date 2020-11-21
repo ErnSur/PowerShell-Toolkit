54ad2f88d96bb81cb17f3c603e5f3f9a87cc76e8
Ernest Suryś <ernest.surys@gmail.com>
2020-11-21 16:21:27 +0100

    added param attributes

--- ES.Unity/ES.Unity.psm1
+++ ES.Unity/ES.Unity.psm1
@@ -1,4 +1,11 @@
-function Get-UnityProjectVersion($projectPath) {
+function Get-UnityProjectVersion {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $projectPath
+    )
+
     $versionFilePath = Join-Path $projectPath ProjectSettings ProjectVersion.txt
     $result = Select-String -Pattern "\d.*" -Path $versionFilePath | Select-Object -First 1 | %{ $_.Matches[0].Value }
     if(!$result) {
