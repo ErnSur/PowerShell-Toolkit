71290e6bf5505b9006a17b0b0258cf5b06434fab
Ernest Suryś <ernest.surys@gmail.com>
2021-01-21 22:25:14 +0100

    Added format to get-adbdevies

--- ES.Android/ES.Android.psm1
+++ ES.Android/ES.Android.psm1
@@ -48,16 +48,22 @@ function Get-AdbDevices {
         return $text.substring(0, 1).ToUpper() + $text.substring(1)
     }
 
+    function New-AdbDevice($properties) {
+        $device = [PSCustomObject]$properties
+        $device.PSTypeNames.Insert(0,'ADB.Device')
+        return $device
+    }
+
     $result = adb devices -l | Where-Object { $_.trim() -ne "" } | Select-Object -Skip 1 | ForEach-Object {
         $tsv = $_ -replace '\ \ +', ' Status:'
         $tsv = $tsv -replace '\ ', "`n"
         $tsv = "Id:" + $tsv
         $tsv = $tsv.Split() | ForEach-Object { FirstCharToUpper($_) } | Join-String -Separator "`n"
         $props = ConvertFrom-StringData -Delimiter ':' -StringData $tsv
-        return [PSCustomObject]$props
+        return New-AdbDevice $props
     }
-    
-    @($result)
+
+    return @($result)
 }
 
 function Uninstall-AndroidApp {
@@ -182,4 +188,5 @@ function Initialize-SDKPath {
         return
     }
 }
+Update-TypeData -TypeName "ADB.Device" -DefaultDisplayPropertySet 'Id', 'Status', 'Model', 'Product' -Force
 Initialize-SDKPath
