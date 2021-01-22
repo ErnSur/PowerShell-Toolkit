83908460c4d87850d0b62881cf3592c8f0f0d893
Ernest Suryś <ernest.surys@gmail.com>
2021-01-22 06:01:53 +0100

    Get-UnityProjectVersion cleanup

--- ES.Unity/ES.Unity.psm1
+++ ES.Unity/ES.Unity.psm1
@@ -1,13 +1,19 @@
 function Get-UnityProjectVersion {
-    [CmdletBinding()]
     param
     (
-        [Parameter(Mandatory=$true)]
-        [string] $projectPath
+        [Parameter(Mandatory)]
+        [string] $ProjectPath
     )
 
-    $versionFilePath = Join-Path $projectPath ProjectSettings ProjectVersion.txt
-    $result = Select-String -Pattern "\d.*" -Path $versionFilePath | Select-Object -First 1 | %{ $_.Matches[0].Value }
+    $versionFilePath = Join-Path $ProjectPath ProjectSettings ProjectVersion.txt
+    if(!(Test-Path $versionFilePath -PathType Leaf)) {
+        Write-Error "Could not find project version at: $versionFilePath"
+        return
+    }
+
+    $result = Select-String -Pattern "\d.*" -Path $versionFilePath | Select-Object -First 1 |
+    ForEach-Object { $_.Matches[0].Value }
+
     if(!$result) {
         Write-Output "No Version found."
     }
